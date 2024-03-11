8556192fa8abe40009306fd8de6b0a8db96cb881
Ole Petter Bang <ole.petter.bang@onyxcentersource.com>
2024-03-11 14:57:36 +0100

    Add Build Bridges kata.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Kata
+{
+    record Point(int X, int Y);
+    record Line(Point From, Point To);
+
+    class Program
+    {
+        static Line[] SolvePuzzle(int[,] board)
+        {
+            var lines = new List<Line>();
+
+            // Non-zero board cells represent islands that should have the corresponding
+            // number of bridges connecting them to their horizontal/vertical neighbors,
+            // without crossing any other islands or bridges, with maximum two bridges
+            // between any two islands, interconnecting all islands.
+            //
+            // Only need bridges in a single direction, e.g. only (x1, y1) -> (x2, y2) and 
+            // not (x2, y2) -> (x1, y1) in addition.
+            //
+            // How to add bridge/lines, e.g. from (0,0) to (0, 2): lines.Add(new(new(0, 0), new(0, 2)));
+
+            // TODO: Calculate bridge locations as lines between islands ((x1, y1) -> (x2, y2))            
+            
+            return lines.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            var board = new int[7, 7] {
+                { 4, 0, 4, 0, 3, 0, 2 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 2, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 5, 0, 1, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 3, 0, 2, 0, 5, 0, 3 }
+            };
+            
+            var lines = SolvePuzzle(board);
+
+            // Expected lines from top-right 4:
+            // (0, 0) -> (0, 2) to right 4
+            // (0, 0) -> (0, 2) to right 4
+            // (0, 0) -> (4, 0) to below 5
+            // (0, 0) -> (4, 0) to below 5
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine($"({line.From.X}, {line.From.Y}) -> ({line.To.X}, {line.To.Y})");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# kata
+# Code Kata
+
+## Build Bridges
+
+This code kata involves connecting "islands" with a corresponding number of "bridges"
+(as stated on the islands/nodes), e.g. a node with a "4" should have a total of 4
+bridges/links to other islands/nodes.
+
+The first picture shows an example board before any bridges have been built:
+
+![Board before bridges are built](./Images/BridgesBefore.png)
+
+The second picture shows the solution to the above example board after building all bridges:
+
+![Board before bridges are built](./Images/BridgesAfter.png)
+
+The provided code suggests how to model the board. To run the code, simply execute `dotnet run`.
+
+### Rules
+
+* Connect all **islands** with the corresponding number of **bridges**
+* Any two islands may have a maximum of two bridges between them
+* Bridges can only be horizontal and vertical
+* Bridges cannot cross other islands or bridges
+* Final solution should interconnect all islands in a single graph
+
+### Solution Submission
+Please submit your solution as a zip file to puzzle[at]onyxcentersource.com by March 15th 2024 at 12:00 (PM).
